c1c1c3abcdd412268d1a0cd7df3f85fd083ea6e2
Mark <mark@stengle.in>
2017-07-05 23:24:45 -0400

    Open Excel file and Start Processing
    Currently hags on open...not sure what the issue is. Theres a
    comment in gmuImport.go that shows where it hangs at.
    
    Signed-off-by: Mark Stenglein <mark@stengle.in>

--- gmuImport.go
+++ gmuImport.go
@@ -2,12 +2,55 @@
 package schedules
 
 import (
-//	"fmt"
-//"os"
-//
-//"github.com/Luxurioust/excelize"
+	"errors"
+	"github.com/tealeg/xlsx"
+	"os"
 )
 
-func GmuImport(files []string) {
+const sheetName = "Sheet1"
 
+func GmuImport(files []string) {
+	initLogging()
+	startDatabase()
+	defer database.Close()
+
+	for _, file := range files {
+		importFile(file)
+	}
+}
+
+func importFile(file string) {
+	log.Infof("Opening excel file: %s", file)
+	xlFile, err := xlsx.OpenFile(file)
+	if err != nil {
+		log.Criticalf("File \"%s\" failed to open!", file)
+		log.Critical(err)
+		os.Exit(1)
+	}
+
+	// currently hangs right here....not sure why
+
+	log.Infof("%s opened successfully.", file)
+
+	headerRow, err := locateHeader(xlFile)
+	if err != nil {
+		log.Critical(err)
+		os.Exit(1)
+	}
+	log.Infof("Header row is: %d", headerRow)
+}
+
+func locateHeader(xlFile *xlsx.File) (int, error) {
+	for i := 1; i <= 40; i++ {
+		currentCell := xlFile.Sheets[0].Cell(i, 1)
+		currentValue, err := currentCell.String()
+		if err != nil {
+			return 0, err
+		}
+		log.Infof("| %d | %s |", i, currentValue)
+		if currentValue == "COURSE" {
+			return i, nil
+		}
+	}
+	return 0, errors.New("Valid header not found")
 }
