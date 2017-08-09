a797c635a9af0412f13ecbb153003e26126bc3f2
Mark Stenglein <mark@stengle.in>
2017-08-09 19:47:49 -0400

    Adds simple Get functions to query database for Sections
    Signed-off-by: Mark Stenglein <mark@stengle.in>

--- section.go
+++ section.go
@@ -2,12 +2,45 @@ package schedules
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
 // Section Collections
+
+// Queries the database for a particular section in a given semester.
+func GetSectionByCRN(crn int, semester string) (Section, error) {
+	if database == nil {
+		return Section{}, errors.New("Database not yet initialized")
+	}
+	var response []Section
+	database.Where(&Section{CRN: crn, Semester: semester}).Find(&response)
+	if len(response) > 1 {
+		return Section{}, errors.New("Multiple matches found")
+	}
+	switch length := len(response); {
+	case length > 1:
+		return Section{}, errors.New(fmt.Sprintf("Multiple matches found with crn: %d", crn))
+	case length == 0:
+		return Section{}, errors.New(fmt.Sprintf("Section %d does not exist", crn))
+	}
+	return response[0], nil
+}
+
+// Queries the database for a list of sections in a given semester.
+func GetSectionsByCRN(crns []int, semester string) ([]Section, error) {
+	var sections []Section
+	for _, crn := range crns {
+		newSection, err := GetSectionByCRN(crn, semester)
+		if err != nil {
+			return []Section{}, err
+		}
+		sections = append(sections, newSection)
+	}
+	return sections, nil
+}
 
 func HandleSectionIndex(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	var sections []Section
